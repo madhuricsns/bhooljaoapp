f40737ceaa32bc6cad6b5a4b47ff8b636a7be8b2
Padmajacsns <109515035+Padmajacsns@users.noreply.github.com>
2023-11-02 12:19:11 +0530

    update

--- application/models/Common_Model.php
+++ application/models/Common_Model.php
@@ -27,6 +27,15 @@
 			}
 			return $randomString;
 		}
+		function randomCode($length = 4) {
+			$characters = '0123456789';
+			$charactersLength = strlen($characters);
+			$randomString = '';
+			for ($i = 0; $i < $length; $i++) {
+				$randomString .= $characters[random_int(0, $charactersLength - 1)];
+			}
+			return $randomString;
+		}
         function otp($length = 4) {
 			$characters = '0123456789';
 			$charactersLength = strlen($characters);
